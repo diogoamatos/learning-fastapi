e85d39d21e1657cf1328372054eb1090cec39752
Diogo Matos <alvesdiogo.mts@gmail.com>
2023-09-01 19:10:17 -0300

    Opcoes dentro do modelo Pydantic

--- schemas.py
+++ schemas.py
@@ -1,11 +1,17 @@
 from enum import Enum
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, HttpUrl
 
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
+
+# Pydantic valida strings de URL automaticamente com HttpUrl
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
 
 
 # Assim como Query e Path, Field adiciona validaçao e metadata dentro dos
@@ -16,6 +22,28 @@ class Item(BaseModel):
         default=None, title="a descricao do item", max_length=300
     )
     price: float = Field(gt=0, description="o preco deve ser maior que zero")
+    # pode-se utilizar modelos pydantic como subtipos por meio de list
+    # (python3.10+) e caso seja necessario que os items sejam unicos
+    # (se for tags por exemplo) é só utilizar:
+    tags: set[str] = set()
+    # images: list[Image] | None = None
+
+    # Pode-se declarar exeplos para o Pydantic incluir no schema JSON
+    # da documentacao utilizando model_config ou os exemplos podem ser
+    # declarados inline de cada variavel exemplo:
+    # name: str = Field(examples=["Foo"])
+    # description: str | None = Field(default=None, examples=["Um item bem maneiro"]
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    'name': 'Foo',
+                    'description': 'Um item bem maneiro',
+                    'price': 13.1
+                }
+            ]
+        }
+    }
 
 
 data1 = {
